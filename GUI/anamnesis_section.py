183aff00983d8b11a59102c5ca9b70dea76599a4
TimMazh <tim@mazhari.ch>
2025-04-14 11:33:23 +0200

    Anamnesis medication procedure and secondary diagnosis tab added

---
+++ GUI/anamnesis_section.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import scrolledtext
+
+class AnamnesisSection:
+    def __init__(self, notebook, anamnesis_tab, doctors_letter):
+        self.doctors_letter = doctors_letter
+        # Notebook für Tabs erstellen
+        self.notebook = notebook
+        self.anamnesis_tab = anamnesis_tab
+        self.anamnesis_tab.pack(fill=tk.BOTH, expand=True)
+
+
+    def create_anamnesis_text_tab(self):
+        # Tab für Nebendiagnosetext erstellen
+        self.notebook.add(self.anamnesis_tab, text="Anamnese")
+
+        # Freitextfeld für den Nebendiagnosetext
+        text_frame = ttk.LabelFrame(self.anamnesis_tab, text="Anamnese", padding=10)
+        text_frame.pack(fill=tk.BOTH, expand=True, padx=10, pady=10)
+
+        self.diagnosis_text = scrolledtext.ScrolledText(text_frame, wrap=tk.WORD, height=10)
+        self.diagnosis_text.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
+
+        # Binde das Event für Änderungen im Textfeld
+        self.diagnosis_text.bind("<<Modified>>", self.on_text_change)
+
+        # Standardtext initialisieren
+        self.load_initial_text()
+
+    def load_initial_text(self):
+        """
+        Lädt den initialen Text aus self.doctors_letter["diagnosis"] in das Textfeld.
+        """
+        initial_text = self.doctors_letter.get("anamnesis", "")
+        self.diagnosis_text.insert(tk.END, initial_text)
+
+    def on_text_change(self, event):
+        """
+        Wird aufgerufen, wenn der Text im Freitextfeld geändert wird.
+        """
+        # Entferne das Modified-Flag, um wiederholte Events zu vermeiden
+        self.diagnosis_text.edit_modified(False)
+
+        # Aktualisiere den Wert in self.doctors_letter["diagnosis"]
+        new_text = self.diagnosis_text.get("1.0", tk.END).strip()
+        self.doctors_letter["anamnesis"] = new_text
+        print(self.doctors_letter["anamnesis"])  # Debug-Ausgabe
+
